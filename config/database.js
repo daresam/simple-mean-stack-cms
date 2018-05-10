dfbae8aa69e1337c17f4224d5572faee1a2f2622
Dare Samuel <dawakesam@gmail.com>
2018-05-10 14:54:55 +0100

    Ready to deploy to Heroku

--- config/database.js
+++ config/database.js
@@ -4,21 +4,9 @@ if(process.env.NODE_ENV == 'production') {
     };
 }else {
     module.exports = {
-        database: 'mongodb://root:password@ds245548.mlab.com:45548/meanauth',
+        database: 'mongodb://localhost:27017/meanauth',
         secret: 'yoursecret'
     };
 }
-
-
-// if(process.env.NODE_ENV == 'production') {
-//     module.exports = {
-//         database: 'mongodb://root:password@ds245548.mlab.com:45548/meanauth',
-//     };
-// }else {
-//     module.exports = {
-//         database: 'mongodb://localhost:27017/meanauth',
-//         secret: 'yoursecret'
-//     };
-// }
 
 
